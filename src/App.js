5c01151f1e1427250fc8b23f74f242a2e4e66eba
Anant Mishra <84588156+Anant-mishra1729@users.noreply.github.com>
2021-09-07 19:19:29 +0530

    Few changes

--- src/App.js
+++ src/App.js
@@ -3,15 +3,14 @@ import "./App.css";
 import React, { Component } from "react";
 import Navbar from "./components/Navbar";
 import News from "./components/News";
-import Newscarousle from "./components/Newscarousle";
 
 export default class App extends Component {
   render() {
     return (
       <div>
         <Navbar title="News Daily" />
-        <Newscarousle />
-        {/* <News /> */}
+        {/* <Newscarousle /> */}
+        <News />
       </div>
     );
   }
--- src/components/News.js
+++ src/components/News.js
@@ -4,23 +4,31 @@ import NewItem from "./NewsItem";
 export class News extends Component {
   render() {
     return (
-      <div className="container my-2">
-        <div class="card bg-dark text-white">
-          <img
-            class="card-img"
-            src="https://www.reuters.com/resizer/72oS_kjyBkx-BPqHrv9I5JHhFD4=/1200x628/smart/filters:quality(80)/cloudfront-us-east-2.images.arcpublishing.com/reuters/W2ZNKAQLPJNHVPECUTSGVTN2YE.jpg"
-            alt="Card image"
-            width="100px"
-            height="270px"
-          />
-          <div class="card-img-overlay">
-            <h5 class="card-title">Card title</h5>
-            <p class="card-text">
-              This is a wider card with supporting text below as a natural
-              lead-in to additional content. This content is a little bit
-              longer.
-            </p>
-            <p class="card-text">Last updated 3 mins ago</p>
+      <div>
+        <div className="container my-3 mx-0">
+          <h2>Top Headlines</h2>
+          <div className="row">
+            <div className="col-md-4 my-2">
+              <NewItem title="News title" desc="mydesc" />
+            </div>
+            <div className="col-md-4 my-2">
+              <NewItem title="News title" desc="mydesc" />
+            </div>
+            <div className="col-md-4 my-2">
+              <NewItem title="News title" desc="mydesc" />
+            </div>
+          </div>
+
+          <div className="row">
+            <div className="col-md-4">
+              <NewItem title="News title" desc="mydesc" />
+            </div>
+            <div className="col-md-4">
+              <NewItem title="News title" desc="mydesc" />
+            </div>
+            <div className="col-md-4">
+              <NewItem title="News title" desc="mydesc" />
+            </div>
           </div>
         </div>
       </div>
